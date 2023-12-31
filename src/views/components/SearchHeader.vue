7ccf0c0e534e0d58efe30d2cf6966141dcf4ec62
Jaquie <jaq.cat@protonmail.ch>
2023-12-31 12:55:06 -0300

    header

--- src/views/components/SearchHeader.vue
+++ src/views/components/SearchHeader.vue
@@ -10,6 +10,11 @@
                 <img alt="Search" src="@/assets/search.svg">
             </button>
         </form>
+        <section id="stuff">
+            <RouterLink to=""><img src="@/assets/pencil.png" alt="Create"></RouterLink>
+            <RouterLink to=""><img src="@/assets/mailbox.png" alt="Notifications"></RouterLink>
+            <RouterLink to=""><img src="@/assets/circle.png" alt="Profile"></RouterLink>
+        </section>
     </header>
 </template>
   
@@ -22,26 +27,24 @@ header {
     background-color: $main;
     width: 100vw;
     padding: 1rem;
+
+    .logo {
+        margin: 0 1rem 0 0;
+    }
 }
 
-.logo {
-    margin: 0 1rem 0 0;
-}
-
-#search-box:focus-within {
-    outline: 1px solid $accent;
+#search-box, #stuff {
+    height: 2rem;
+    display: flex;
 }
 
 #search-box {
-    font-size: 1.1rem;
-    height: 1.6rem;
-    display: flex;
-
     input {
         height: 100%;
         width: 40vw;
         border: 0;
         padding: 4px;
+        font-size: 1.2rem;
     }
 
     input:focus {
@@ -50,14 +53,30 @@ header {
 
     button {
         height: 100%;
-        padding: 0.2rem;
+        padding: 0.22rem;
         border: 0;
         display: flex;
         flex-direction: column;
         justify-content: center;
+        background-color: $accent;
         img {
             height: 100%;
         }
+    }
+}
+
+#search-box:focus-within {
+    outline: 2px solid $accent;
+}
+
+#stuff {
+    margin-left: auto;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 0.5rem;
+
+    a, img {
+        height: 120%;
     }
 }
 </style>
