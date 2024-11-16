7b6ef92318800f3e8b2bebc167c72f0cc20d081c
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-11-15 22:24:53 -0500

    refactor(infra/repositories): update post types
    add proper `Post` response type

--- src/infrastructure/repositories/Post/post.ts
+++ src/infrastructure/repositories/Post/post.ts
@@ -1,11 +1,11 @@
-import type { Paginate } from "@/infrastructure/api/types"
+import type { Paginate } from "@infra/api/types.js"
 import { HTTP, callAPI } from "../../api"
-import type { PostCreateBody, PostGetManyBody, UpdatePostBody, PostDeleteResponse } from "./post.types"
+import type { PostCreateBody, PostGetManyBody, UpdatePostBody, PostDeleteResponse, Post } from './post.types'
 
 export abstract class PostRepository {
     static create = (body: PostCreateBody) => callAPI<{}>('post', HTTP.POST, body)
     static get = (id: string) => callAPI(`post/${id}`, HTTP.GET)
-    static getMany = (body: PostGetManyBody) => callAPI<Paginate<unknown>>(`post`, HTTP.GET, body)
-    static update = (id: string, body: UpdatePostBody) => callAPI(`post/${id}`, HTTP.PUT, body)
+    static getMany = (body?: PostGetManyBody) => callAPI<Paginate<Post>, PostGetManyBody>(`post`, HTTP.GET, body)
+    static update = (id: string, body: UpdatePostBody) => callAPI<{}, UpdatePostBody>(`post/${id}`, HTTP.PUT, body)
     static delete = (id: string) => callAPI<PostDeleteResponse>(`post/${id}`, HTTP.DELETE)
 }
--- src/infrastructure/repositories/Post/post.types.ts
+++ src/infrastructure/repositories/Post/post.types.ts
@@ -1,27 +1,61 @@
 export type PostCreateBody = {
-    title: string
-    text: string
-    media: File
-    thumb?: File
-    nsfw?: boolean
+  title: string
+  text: string
+  media: File
+  thumb?: File
+  nsfw?: boolean
+  tags: string[]
+  credits?: PostCredit[]
+  minTierId?: string
+  seriesId?: string
+}
+
+type PostCredit = {
+  userId: string
+  description: string
+}
+
+export type PostAuthor = {
+  id: string
+  nickname: string
+  profilePhotoUrl: string
+}
+
+export type Post = {
+  text: string
+
+  meta: {
+    author: PostAuthor
+    credits: PostCredit[]
+    likes: number
+    dislikes: number
+    views: number
+    myVote: number
+    minTierId: string;
     tags: string[]
+    nsfw: boolean
+    width: number
+    height: number
+  }
+  thumbUrl: string
 }
 
 export type SortBy = 'latest' | 'oldest' | 'high-views' | 'low-views'
 export type PostGetManyBody = {
-    userTierId?: string
-    creatorId?: string
-    sortBy: SortBy
-    titleSearch?: string,
-    nsfw: boolean
-    page: number
-    size: number
+  userTierId?: string
+  creatorId?: string
+  sortBy: SortBy
+  titleSearch?: string
+  nsfw: boolean
+  page: number
+  size: number
 }
 
 export type UpdatePostBody = {}
 
 export enum PostDeleteStatus {
-    POST_DELETED = 'Post Deleted',
-    AUTHOR_REMOVED = 'Author Removed'
+  POST_DELETED = 'Post Deleted',
+  AUTHOR_REMOVED = 'Author Removed'
 }
+
 export type PostDeleteResponse = { status: PostDeleteStatus }
