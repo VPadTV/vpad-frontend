a15e169fe483c87a74b1e7acab572614fe3966d6
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-11-15 22:26:44 -0500

    refactor(views): update homepage with actual content

--- src/modules/shared/views/HomeView.vue
+++ src/modules/shared/views/HomeView.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+//
+import { PostRepository } from "@infra/repositories/Post/post";
+import { useRoute } from 'vue-router'
+import type { Post } from '@infra/repositories/Post/post.types.js'
+const route = useRoute()
+
+const {data} = (await PostRepository.getMany({
+  nsfw: false,
+  page: parseInt((route.query.page as string) ?? "1"),
+  size: 50,
+  sortBy: 'latest'
+}))!
+</script>
 <template>
-    <h1>Not Implemented</h1>
+  <a-list :grid="{gutter: 8, sm: 1, md: 4, lg: 5, xl: 6, xxl: 8}" :data-source="data as Post[]">
+    <template #renderItem="{item}">
+      <a-list-item>
+       <a-card>
+         <template #cover>
+           <img :src="item.thumbUrl" :alt="item.text"/>
+         </template>
+       </a-card>
+      </a-list-item>
+    </template>
+  </a-list>
+  <h1>Not Implemented</h1>
 </template>
