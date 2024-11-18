c63bc62fe49b9560802357a093ae42c266c87fee
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-11-18 14:54:32 -0500

    refactor(infra/repositories): make `UserGetManyBody` argument optional

--- src/infrastructure/repositories/User/user.ts
+++ src/infrastructure/repositories/User/user.ts
@@ -5,6 +5,6 @@ import type { UserEditBody, UserGetManyBody, UserGetManyResponse, UserGetRespons
 const BASE = 'user'
 export abstract class UserRepository {
     static get = async (id: string) => await callAPI<UserGetResponse>(`${BASE}/${id}`, HTTP.GET)
-    static getMany = async (body: UserGetManyBody) => await callAPI<UserGetManyResponse>(`${BASE}`, HTTP.GET, body)
+    static getMany = async (body?: UserGetManyBody) => await callAPI<UserGetManyResponse>(`${BASE}`, HTTP.GET, body)
     static update = async (id: string, body: UserEditBody) => callAPI<{}>(`${BASE}/${id}`, HTTP.PUT, asFormData(body))
 }
