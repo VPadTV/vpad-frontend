af568a797b24c0d6db38a1c52df0086988eae8ae
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-11-20 15:39:06 -0500

    refactor(app): add icon components to global scope

--- src/plugins/ui/index.ts
+++ src/plugins/ui/index.ts
@@ -1,10 +1,13 @@
 import { addToastDependencies } from './toast'
 import { addLazyLoadDependencies } from './lazyLoad'
 import Antd from "ant-design-vue";
+import * as AntIcons from "@ant-design/icons-vue";
 import type { App } from 'vue'
 
 export function addUIDependencies(app: App) {
   addLazyLoadDependencies(app)
   addToastDependencies(app)
+  for(let [k, T] of Object.entries(AntIcons))
+    app.component(k, T)
   app.use(Antd)
 }
