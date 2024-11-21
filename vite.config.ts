9c09a5d3b6317f872eff3f7a32a85fb2af856b24
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-11-21 18:09:40 -0500

    chore(app): update vite config
    add all variables defined in .env to `defines`, regardless of prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,46 @@
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from "tailwindcss"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@shared': fileURLToPath(new URL('./src/modules/shared', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
-      '@plugins': fileURLToPath(new URL('./src/plugins', import.meta.url)),
-      '@domain': fileURLToPath(new URL('./src/domain', import.meta.url)),
-      '@infrastructure': fileURLToPath(new URL('./src/infrastructure', import.meta.url)),
-      "@infra": fileURLToPath(new URL('./src/infrastructure', import.meta.url))
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@shared': fileURLToPath(new URL('./src/modules/shared', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
+        '@plugins': fileURLToPath(new URL('./src/plugins', import.meta.url)),
+        '@domain': fileURLToPath(new URL('./src/domain', import.meta.url)),
+        '@infrastructure': fileURLToPath(new URL('./src/infrastructure', import.meta.url)),
+        "@infra": fileURLToPath(new URL('./src/infrastructure', import.meta.url))
+      }
+    },
+    define: env,
+    build: {
+      minify: true,
+      cssMinify: true,
+      cssCodeSplit: true
+    },
+    css: {
+      postcss: {
+        plugins: [tailwindcss()]
+      }
+    },
+    assetsInclude: ['src/assets/images/**/*', 'src/assets/icons/**/*'],
+    // css : {
+    //   preprocessorOptions : {
+    //     scss: {
+    //       additionalData: `@import "src/assets/styles/main.scss";`
+    //     }
+    //   }
+    // },
+    server: {
+      port: 3001
     }
-  },
-  build: {
-    minify: true,
-    cssMinify: true,
-    cssCodeSplit: true
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss()]
-    }
-  },
-  assetsInclude: ['src/assets/images/**/*', 'src/assets/icons/**/*'],
-  // css : {
-  //   preprocessorOptions : {
-  //     scss: {
-  //       additionalData: `@import "src/assets/styles/main.scss";`
-  //     }
-  //   }
-  // },
-  server: {
-    port: 3001
   }
 })
